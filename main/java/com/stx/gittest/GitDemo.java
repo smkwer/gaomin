e59f499371b481bf64b2f96eef431a7d1af7cfd2
gavin <gavin@stx.com>
2020-12-01 19:17:33 +0800

    git test init

---
+++ main/java/com/stx/gittest/GitDemo.java
@@ -0,0 +1,12 @@
+package com.stx.gittest;
+
+/**
+ * @Aythor :张国军
+ * @Date ：Created in 19:11 2020/12/1
+ * @Description : gittest1
+ **/
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
