e858c114bcf9a1445005dd6e713ee2d861ac0e04
gavin <gavin@stx.com>
2020-12-01 19:38:12 +0800

    update gittest

--- main/java/com/stx/gittest/GitDemo.java
+++ main/java/com/stx/gittest/GitDemo.java
@@ -8,5 +8,6 @@ package com.stx.gittest;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello Git!");
+        System.out.println("hello Git1");
     }
 }
